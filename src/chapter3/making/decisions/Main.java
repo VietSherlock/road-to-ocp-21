34d030b3b4e70b7ff29b44286e9b1f8b9be3ffc9
Viet Le Nguyen Ngoc <viet.lenguyenngoc@emea.nttdata.com>
2025-02-25 18:30:40 +0700

    Chapter3: switch expression with Yield and PatternMatching

---
+++ src/chapter3/making/decisions/Main.java
@@ -0,0 +1,9 @@
+package chapter3.making.decisions;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(SwitchExample.switchExpressionWithYield(5, 9));
+        System.out.println(SwitchExample.swithExpressionWithPatternMatching(1.2));
+    }
+}
---
+++ src/chapter3/making/decisions/SwitchExample.java
@@ -0,0 +1,32 @@
+package chapter3.making.decisions;
+
+public class SwitchExample {
+
+    public static String swithExpressionWithPatternMatching(Number height) {
+        //example for switch expression with Pattern Matching
+        return switch (height) {
+            case Integer i -> "Rounded: " + i;
+            case Double i -> "Precise: " + i;
+            case Number i -> "Unknown: " + i;
+        };
+    }
+
+    public static String switchExpressionWithYield(int fish, int length) {
+        //switch expression must be exhaustive
+        //it means all cases have to covered
+        return switch (fish) {
+            case 1 -> "Goldfish";       //this is case expression
+            case 2 -> {yield "Trout";}  //this is case block
+            case 3 -> {
+                if (length > 10)
+                    yield "Blowfish";   //yield acted as return statement in case block
+                else
+                    yield "Green";
+            }
+            case 4 -> {
+                throw new RuntimeException("Unsupported value");
+            }
+            default -> "Swordfish";
+        };
+    }
+}
